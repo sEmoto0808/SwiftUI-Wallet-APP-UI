77eff5b8672df7eac58748de85d5efdb1ff69d42
Sho Emoto <dd2d00seg1@gmail.com>
2022-02-27 21:05:34 +0900

    add project

---
+++ SwiftUI-Wallet-App/Shared/SwiftUI_Wallet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Wallet_AppApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Wallet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
